c51e8993140328feba38d8f79d64724e4c2ca544
SeanRichhh <sean-richards@outlook.com>
2022-05-05 18:24:17 +0100

    core done

--- src/App.js
+++ src/App.js
@@ -1,56 +1,87 @@
 import "./App.css"
+import {useState} from "react"
+
 
 function App() { 
+// NumOne is the variable storing the current value
+// setNumberOne is a function that accepts a new value, which then passes to the numOne
+  const [numOne, setNumberOne] = useState(0)
+  // opSign is the variable storing the current value
+ // setOpSign is a function that accepts a new value, which then passes to the opSign
+  
+  const [opSign, setOpSign] = useState("+")
+// NumTwo is the variable storing the current value
+// setNumberTwo is a function that accepts a new value, which then passes to the numTwo
+  const [numTwo, setNumberTwo] = useState(0)
+  const [sumOf, setSumOf] = useState(0)
+
+  const sumOfCalucator = () => {
+    if (opSign === "+") {
+      setSumOf(Number(numOne) + Number(numTwo));
+    }
+    if (opSign === "-") {
+      setSumOf(Number(numOne) - Number(numTwo));
+    }
+    if (opSign === "*") {
+      setSumOf(Number(numOne) * Number(numTwo));
+    }
+    if (opSign === "÷") {
+      setSumOf(Number(numOne) / Number(numTwo));
+    }
+  };
+
+
 
   return (
     <div className="calculator">
         <div className="panel">
-          <p>0</p>
+          <p>{numOne}</p>
           <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
+            <button onClick={() => setNumberOne(1)}>1</button>
+            <button onClick={() => setNumberOne(2)}>2</button>
+            <button onClick={() => setNumberOne(3)}>3</button>
+            <button onClick={() => setNumberOne(4)}>4</button>
+            <button onClick={() => setNumberOne(5)}>5</button>
+            <button onClick={() => setNumberOne(6)}>6</button>
+            <button onClick={() => setNumberOne(7)}>7</button>
+            <button onClick={() => setNumberOne(8)}>8</button>
+            <button onClick={() => setNumberOne(9)}>9</button>
+            <button onClick={() => setNumberOne(0)}>0</button>
+            <button onClick={() => setNumberOne("")}>Clear</button>
           </div>
         </div>
         
         <div className="panel">
-          <p>+</p>
+          <p>{opSign}</p>
           <div className="numbers">
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>÷</button>
+            <button onClick={() => setOpSign("+")}>+</button>
+            <button onClick={() => setOpSign("-")}>-</button>
+            <button onClick={() => setOpSign("*")}>*</button>
+            <button onClick={() => setOpSign("÷")}>÷</button>
+            <button onClick={() => setOpSign("")}>clear</button>
           </div>
         </div>
 
         <div className="panel">
-          <p>0</p>
+          <p>{numTwo}</p>
           <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
+          <button onClick={() => setNumberTwo(1)}>1</button>
+            <button onClick={() => setNumberTwo(2)}>2</button>
+            <button onClick={() => setNumberTwo(3)}>3</button>
+            <button onClick={() => setNumberTwo(4)}>4</button>
+            <button onClick={() => setNumberTwo(5)}>5</button>
+            <button onClick={() => setNumberTwo(6)}>6</button>
+            <button onClick={() => setNumberTwo(7)}>7</button>
+            <button onClick={() => setNumberTwo(8)}>8</button>
+            <button onClick={() => setNumberTwo(9)}>9</button>
+            <button onClick={() => setNumberTwo(0)}>0</button>
+            <button onClick={() => setNumberTwo("")}>Clear</button>
           </div>
         </div>
         <div className="panel answer">
-          <p>0</p>
+          <p>{sumOf}</p>
           <div>
-            <button>=</button>
+            <button onClick={() => sumOfCalucator("")}>=</button>
           </div>
         </div>
     </div>
